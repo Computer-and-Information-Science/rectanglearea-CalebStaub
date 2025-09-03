591552d1e11b541db43ef21dc107a03ff7b4a901
Caleb Staub <cstaub138@gmail.com>
2025-09-03 15:41:33 +0000

    Rectangle Properties

--- main.cpp
+++ main.cpp
@@ -4,6 +4,14 @@ using namespace std;
 int main() {
   const double length = 7.2;
   const double width = 1.3;
-
+  
+  double area = length * width;
+  double perimeter = 2 * (length + width);
+  
+  cout<< "Rectangle Properties:"<<endl;
+  cout<< "Length = "<< length << endl;
+  cout<< "Width = " << width << endl;
+  cout<< "Area = " << area << endl;
+  cout<< "Perimeter = " << perimeter << endl;
 
 }
